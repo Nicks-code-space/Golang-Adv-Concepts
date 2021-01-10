dac018edbdd41b4409b39216eb12e9f12cc1c35a
Nicks-code-space <rcravistein@yahoo.in>
2021-01-10 06:09:15 -0500

    add middleware for request validation

--- utils/helpers.go
+++ utils/helpers.go
@@ -14,3 +14,4 @@ log.Info().Str("method", r.Method).Msg("request received")
 if err != nil {
 	return nil, fmt.Errorf("db query failed: %w", err)
 }
+rows, err := db.QueryContext(ctx, query, args...)
