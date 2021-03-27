c706a417f6be1eb5470b9ea35cee1cdc71c95ec7
Nicks-code-space <rcravistein@yahoo.in>
2021-03-27 14:57:58 -0400

    fix: handle timeout errors in grpc client

--- service/user.go
+++ service/user.go
@@ -6,3 +6,6 @@ metrics.RequestCount.WithLabelValues(route).Inc()
 if err != nil {
 	return nil, fmt.Errorf("db query failed: %w", err)
 }
+if err != nil {
+	return nil, fmt.Errorf("db query failed: %w", err)
+}
