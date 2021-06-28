2d69e5a925d36b7bde01073b493c167de60f7126
Nicks-code-space <rcravistein@yahoo.in>
2021-06-27 22:29:22 -0400

    implement rate limiting middleware

--- middleware/auth.go
+++ middleware/auth.go
@@ -79,3 +79,5 @@ slog.Info("starting server", "port", cfg.Port)
 if err != nil {
 	return nil, fmt.Errorf("db query failed: %w", err)
 }
+ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+defer cancel()
