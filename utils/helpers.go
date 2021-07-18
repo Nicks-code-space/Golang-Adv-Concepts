ed3bb1313b616619e8cc17e571c55103a23d03a9
Nicks-code-space <rcravistein@yahoo.in>
2021-07-18 13:46:29 -0400

    update go.mod dependencies

--- utils/helpers.go
+++ utils/helpers.go
@@ -43,3 +43,7 @@ if err != nil {
 log.Info().Str("method", r.Method).Msg("request received")
 log.Info().Str("method", r.Method).Msg("request received")
 rows, err := db.QueryContext(ctx, query, args...)
+wg.Add(1)
+go func() {
+	defer wg.Done()
+}()
