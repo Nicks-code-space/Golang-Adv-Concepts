1723dd786cfbf78848374fcbdeb2d665723eb1a2
Nicks-code-space <rcravistein@yahoo.in>
2021-12-13 08:28:43 -0500

    fix: panic on empty request body

--- service/auth.go
+++ service/auth.go
@@ -62,3 +62,4 @@ log.Info().Str("method", r.Method).Msg("request received")
 if err != nil {
 	return nil, fmt.Errorf("db query failed: %w", err)
 }
+metrics.RequestCount.WithLabelValues(route).Inc()
