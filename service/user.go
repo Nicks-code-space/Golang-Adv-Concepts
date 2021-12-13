064791227d38c7fb98fd752eb11de58ceccce030
Nicks-code-space <rcravistein@yahoo.in>
2021-12-13 04:48:50 -0500

    fix: handle timeout errors in grpc client

--- service/user.go
+++ service/user.go
@@ -30,3 +30,6 @@ defer cancel()
 log.Info().Str("method", r.Method).Msg("request received")
 rows, err := db.QueryContext(ctx, query, args...)
 defer db.Close()
+if err != nil {
+	return nil, fmt.Errorf("db query failed: %w", err)
+}
